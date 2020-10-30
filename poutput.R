10eee4c8a3318b6e29f6ad84c24a1dd184e0ab34
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-10-30 11:23:21 -0500

    Update poutput.R

--- poutput.R
+++ poutput.R
@@ -3,27 +3,34 @@ poutput <- function(df,
                     fs = 15,
                     cw = .75,
                     ch = .25,
-                    font = "Arial",
                     output_type = NULL,
-                    file_name = NULL) {
-  df <- flextable::flextable(df)
+                    file_name = NULL,
+                    font = NULL,
+                    theme = "default") {
+  
+  pobj <- flextable::flextable(df)
+  
+  if (theme == "default"){ 
+    pobj <- poutput.theme(pobj, "default")
+  }
   
   if (length(cw) == 1) {
-    df <- flextable::width(df, width = cw)
+    pobj <- flextable::width(pobj, width = cw)
   } else {
     for (i in seq_along(cw)) {
-      df <- flextable::width(df, j = i, width = cw[[i]])
+      pobj <- flextable::width(pobj, j = i, width = cw[[i]])
     }
   }
   
-  df <- flextable::bold(df, part = "header")
-  df <- flextable::fontsize(df, size = fs, part = "all")
-  df <- flextable::align(df, align = "center", part = "all")
-  df <- flextable::set_caption(df, cap)
-  df <- flextable::font(df, fontname = font, part = "all")
+  pobj <- flextable::fontsize(pobj, size = fs, part = "all")
+  pobj <- flextable::set_caption(pobj, cap)
+  
+  if (!is.null(font)){ 
+    pobj <- flextable::font(pobj, fontname = "Arial", part = "all")
+  }
   
   if (is.null(output_type)) {
-    return(df)
+    return(pobj)
   }
   
   if (is.null(file_name)) {
@@ -35,20 +42,33 @@ poutput <- function(df,
   
   if (output_type == "png") {
     file_name <- paste0(file_name, ".png")
-    flextable::save_as_image(df, path = file_name, webshot = "webshot2")
+    flextable::save_as_image(pobj, path = file_name, webshot = "webshot2")
     print(glue::glue("File saved as {file_name}"))
   }
   
   if (output_type == "pdf") {
     file_name <- paste0(file_name, ".pdf")
-    flextable::save_as_image(df, path = file_name, webshot = "webshot2")
+    flextable::save_as_image(pobj, path = file_name, webshot = "webshot2")
     print(glue::glue("File saved as {file_name}"))
   }
   
   if (output_type == "word") {
     file_name <- paste0(file_name, ".docx")
-    flextable::save_as_docx(df, path = file_name, webshot = "webshot2")
+    flextable::save_as_docx(pobj, path = file_name, webshot = "webshot2")
     print(glue::glue("File saved as {file_name}"))
   }
+  
+}
+
+
+poutput.theme <- function(pobj, theme){ 
+  
+  if (theme == "default") { 
+    pobj <- flextable::bold(pobj, part = "header")
+    pobj <- flextable::align(pobj, align = "center", part = "all")
+    pobj <- flextable::font(pobj, fontname = "Arial", part = "all")
+  }
+  
+  return(pobj)
   
 }
