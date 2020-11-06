02086ad68f8a3a475d823218488f8fbe41edf4dd
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-06 10:58:03 -0600

    Update rate_ratio.R

--- rate_ratio.R
+++ rate_ratio.R
@@ -1,19 +1,19 @@
-#' Calculates rate ratios (using standardized rates) and associated confidence intervals (using population or death amounts and assuming a poisson distribution.) 
+#' Calculates rate ratios (using standardized rates) and associated confidence intervals (using population or death amounts and assuming a poisson distribution.)
 #' @param rates A numeric vector of rates or the name of data.frame columns that contain rates
 #' @param pop A numeric vector of population or death amounts or the name of data frame columns that contain
 #' populations/deaths
-#' @param df If rates and population/deaths are given as column names, the name of the data frame must be 
+#' @param df If rates and population/deaths are given as column names, the name of the data frame must be
 #' specified
 #' @param rr_digits The number of digits the rate ratio should be rounded to
-#' @param digits The number of digits the confidence intervals should be rounded to 
+#' @param digits The number of digits the confidence intervals should be rounded to
 #' @param return_df If you'd like the results returned as a data frame or not. If the inputs are numeric
 #' vectors, return_df will return a 1 by 2 data frame. If inputs are data frame columns, return_df will
-#' return the input data frame with two new columns for rates and CIs added. 
+#' return the input data frame with two new columns for rates and CIs added.
 #' @param conf The confidence interval you'd like
 #' @return A character vector or data frame with rates and confidence intervals
 #' @examples rate_ratio(rates = c(150, 100), pop = c(1000,5000), digits = 3, conf = "95")
 #' @examples rate_ratio(df = rate_ratio_df, rates = c("rate_column_1", "rate_column_2"), pop = c("pop_column_1", "pop_column_2"),
-#` conf = "95", return_df = TRUE, digits = 5) 
+#` conf = "95", return_df = TRUE, digits = 5)
 #' @export
 
 rate_ratio <- function(rates,
@@ -22,23 +22,21 @@ rate_ratio <- function(rates,
                        conf = "95",
                        df,
                        return_df...) {
-  
   conf_cv <- setNames(c(1.65, 1.96, 2.58), c("90", "95", "99"))
-  df_type <- setNames(c("tibble","data.frame"), 
+  df_type <- setNames(c("tibble", "data.frame"),
                       c("tbl_df", "data.frame"))
   UseMethod("rate_ratio")
 }
 
 #' @export
-rate_ratio.list <- function(rates, 
-                            pop, 
-                            digits, 
+rate_ratio.list <- function(rates,
+                            pop,
+                            digits,
                             conf,
                             rr_digits) {
-  
-  for (i in seq_along(rates[-1])) { 
-    rr = round(rates[[i]] / rates[[1]], digits = rr_digits)
-    se = sqrt(1 / pop[[i]] + 1 / pop[[1]])
+  for (i in seq_along(rates[-1])) {
+    rr <- round(rates[[i]] / rates[[1]], digits = rr_digits)
+    se <- sqrt(1 / pop[[i]] + 1 / pop[[1]])
     ul <- round(exp(log(rates[[i]]) + (conf_cv[[conf]] * se)), digits = digits)
     ll <- round(exp(log(rates[[i]]) - (conf_cv[[conf]] * se)), digits = digits)
     return(glue::glue("{rr} ({ul} to {ll})"))
@@ -49,16 +47,15 @@ rate_ratio.list <- function(rates,
 rate_ratio.numeric <- function(rates,
                                pop,
                                digits = 5,
-                               rr_digits = 3, 
+                               rr_digits = 3,
                                conf = "95") {
-  
   rates <- round(rates[[1]] / rates[[2]], digits = rr_digits)
   se <- sqrt(1 / pop[[1]] + 1 / pop[[2]])
   ul <- round(exp(log(rates) + (conf_cv[[conf]] * se)), digits = digits)
   ll <- round(exp(log(rates) - (conf_cv[[conf]] * se)), digits = digits)
   return(glue::glue("{rr} ({ul} to {ll})"))
   
-} 
+}
 
 #' @export
 rate_ratio.character <- function(rates,
@@ -68,7 +65,6 @@ rate_ratio.character <- function(rates,
                                  rr_digits = 3,
                                  conf = "95",
                                  return_df = TRUE) {
-  
   rates <- magrittr::extract(df, rates)
   rates <- round(rates[[1]] / rates[[2]], digits = rr_digits)
   pop <- magrittr::extract(df, pop)
@@ -80,7 +76,8 @@ rate_ratio.character <- function(rates,
     expr_call <- paste0(df_type[[class(df)[1]]], df_cols)
     df <- eval(rlang::parse_expr(expr_call))
   } else {
-    df <- dplyr::mutate(df, Ratio = rates, CI = glue::glue("{ll} - {ul}"))
+    df <-
+      dplyr::mutate(df, Ratio = rates, CI = glue::glue("{ll} - {ul}"))
   }
   return(df)
 }
@@ -91,10 +88,9 @@ rate_ratio.data.frame <- function(rates,
                                   digits = 5,
                                   rr_digits,
                                   conf = "95",
-                                  return_df = TRUE) { 
-  
+                                  return_df = TRUE) {
   #TODO
- 
+  
 }
 
 rate_ratio.tbl_df <- function(rates,
@@ -102,7 +98,6 @@ rate_ratio.tbl_df <- function(rates,
                               df,
                               digits = 5,
                               conf = "95",
-                              return_df = TRUE) { 
- 
-  #TODO 
+                              return_df = TRUE) {
+  #TODO
 }
