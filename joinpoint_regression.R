893bba6da0fc74885d2e59e27066d78f0330eaa7
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-09 13:27:35 -0600

    Update joinpoint_regression.R

--- joinpoint_regression.R
+++ joinpoint_regression.R
@@ -1,1 +1,7 @@
-#TODO
+joinpoint_regression <- function(file, 
+                                 ind_var, 
+                                 AAR, 
+                                 SE) { 
+  
+  
+}
