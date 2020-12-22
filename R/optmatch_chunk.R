99e2ce230a457e4f2b1c29c00363ae5de8f4054e
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-12-21 23:47:39 -0600

    Update optmatch_chunk.R

--- R/optmatch_chunk.R
+++ R/optmatch_chunk.R
@@ -1,10 +1,12 @@
-pairmatch_chunks <- function(d, prev, form, controls = 1){ 
-  if(nrow(d) == 0) { 
+pairmatch_chunks <- function(d, prev, form, controls = 1) {
+  if (nrow(d) == 0) {
     return(prev)
-  } 
-  n <- try(optmatch::pairmatch(as.formula(form), controls = controls, data = d), 
+  }
+  n <- try(optmatch::pairmatch(as.formula(form),
+                               controls = controls,
+                               data = d),
            silent = TRUE)
-  if(class(n)[1] == "optmatch") { 
+  if (class(n)[1] == "optmatch") {
     n <- tibble::enframe(n)
     d <- dplyr::mutate(d, matches = n$value)
     d <- tidyr::drop_na(d)
@@ -13,14 +15,18 @@ pairmatch_chunks <- function(d, prev, form, controls = 1){
   } else {
     return(prev)
   }
-} 
+}
 
-optmatch_chunk <- function(data, formula, controls = 1) { 
+optmatch_chunk <- function(data, formula, controls = 1) {
   data <- LaF::laf_open(LaF::detect_dm_csv(data, header = TRUE))
-  return(LaF::process_blocks(data, fun = pairmatch_chunks, 
-                        form =  formula,
-                        controls = controls,
-                        progress = TRUE))
-
-
-} 
+  return(
+    LaF::process_blocks(
+      data,
+      fun = pairmatch_chunks,
+      form =  formula,
+      controls = controls,
+      progress = TRUE
+    )
+  )
+ 
+}
